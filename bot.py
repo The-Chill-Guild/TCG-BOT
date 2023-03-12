77024dea713646c16e4c801400cbd63f0794f8a7
Phoenix0P <hrudbgharat@gmail.com>
2023-03-12 18:02:38 +0530

    initialize the bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+
+bot = commands.Bot(command_prefix = '.', intents=discord.Intents.all())
+bot_status = cycle(['DM ModMail'])
+
+@tasks.loop(hours=1)
+async def change_status():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="DM ModMail"))
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print('Bot is ready.')
+    change_status.start()
+
+async def main():
+    async with bot:
+        await bot.start(os.getenv('TOKEN')) 
+
+asyncio.run(main())
